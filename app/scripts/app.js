8618db9247297906eb6983ba1fb4d6d559e6aa2a
Jason Hansell <jasonhansell@gmail.com>
2015-12-01 05:12:54 +0000

    updated animation between tabs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,8 +14,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  
-  app.set('card', 0);
 
   app.displayInstalledToast = function() {
     // Check to make sure caching is actually enabled—it won't be in the dev environment.
@@ -24,16 +22,24 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     }
   };
   
-  // event handler to show a toast based on the message provided
-  window.addEventListener('show-toast', function(event) {
-    var toast = document.querySelector('#toast');
-    toast.text = event.detail.displayMessage;
-    toast.show();
-  });
+  app.cardSelected = function(e) {
+    var nap = document.querySelector('neon-animated-pages');
+    var newValue = e.detail.value;
+    var oldValue = nap.selected;
+    if(newValue < oldValue){
+      nap.set('entryAnimation', 'slide-from-left-animation');
+      nap.set('exitAnimation', 'slide-right-animation');
+    } else {
+      nap.set('entryAnimation', 'slide-from-right-animation');
+      nap.set('exitAnimation', 'slide-left-animation');
+    }
+    nap.set('selected', newValue);
+  };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
+    document.querySelector('neon-animated-pages').set('selected', 0);
     document.querySelector('dd-core-card').querySelector('dd-product-chip').set('productIndex', 2);
     document.querySelector('dd-capsule-card').querySelector('dd-product-chip').set('productIndex', 5);
   });
